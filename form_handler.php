27a77a2ccaa5f61f0d4eb65848ee5a45525ced0e
David Hayes <heymrhayes@gmail.com>
2016-09-26 20:47:49 +0000

    rename form_handler

---
+++ form_handler.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Simple Form </title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <h1>Form data posted</h1>
+
+        <?php
+        
+            var_dump($_POST)
+        
+        ?>
+        
+        
+    </div>
+
+
+
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
